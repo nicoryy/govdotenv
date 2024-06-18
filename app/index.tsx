e2079351f51658cb3fdf889cde75bb8fe76d97a8
nicoryy <pedronicory@gmail.com>
2024-06-18 15:53:01 -0300

    nativewind

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,9 @@
 import { Link } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function Login() {
   return (
-    <View style={styles.container}>
+    <View className=''>
       <Text>Login Page</Text>
       <Link href={'/dois'}>Pagina dois</Link>
     </View>
