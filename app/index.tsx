cb63a29132adc958ae69cb7138fefc088ad4ddcb
nicoryy <pedronicory@gmail.com>
2024-06-18 15:05:46 -0300

    expo-router

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,12 @@
+import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export default function Login() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Login Page</Text>
+      <Link href={'/dois'}>Pagina dois</Link>
     </View>
   );
 }
